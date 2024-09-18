9ad0da14be59d8b1e8f5ac661d0f66614122b79d
Nathaniel Daley <ndaley64@gmail.com>
2024-09-17 19:44:17 -0500

    Scale and color background

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,13 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
   update,
 });
 
-function preload() {}
+function preload() {
+  //Scale the canvas while respecting aspect ratio
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+
+  game.stage.backgroundColor = "#eee";
+}
 function create() {}
 function update() {}
