16eb7d19f09d08b7eabf813f47ebecdfd8c19de4
Nathaniel Daley <ndaley64@gmail.com>
2024-09-17 19:58:13 -0500

    Render ball and give it physics

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,19 @@ function preload() {
   game.load.image("ball", "./images/ball.png");
 }
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  //set the ball size and sprite to the ball image set above
   ball = game.add.sprite(50, 50, "ball");
+
+  game.physics.enable(ball, Phaser.Physics.ARCADE);
+
+  //make ball collide with edge of the canvas
+  ball.body.collideWorldBounds = true;
+
+  //make the ball bounce off the bounds of the canvas when it hits them
+  ball.body.bounce.set(1);
+
+  ball.body.velocity.set(150, 150);
 }
 function update() {}
