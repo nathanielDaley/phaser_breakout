8d3fe917ddb3eb3573770bdfe013a0a1258e1125
Nathaniel Daley <ndaley64@gmail.com>
2024-09-17 20:04:13 -0500

    Create paddle and add physics to it

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 });
 
 let ball;
+let paddle;
 
 function preload() {
   //Scale the canvas while respecting aspect ratio
@@ -15,14 +16,22 @@ function preload() {
   game.stage.backgroundColor = "#eee";
 
   game.load.image("ball", "./images/ball.png");
+  game.load.image("paddle", "./images/paddle.png");
 }
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  //set the ball size and sprite to the ball image set above
   ball = game.add.sprite(50, 50, "ball");
+  paddle = game.add.sprite(
+    game.world.width * 0.5,
+    game.world.height - 5,
+    "paddle"
+  );
+
+  paddle.anchor.set(0.5, 1);
 
   game.physics.enable(ball, Phaser.Physics.ARCADE);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
 
   //make ball collide with edge of the canvas
   ball.body.collideWorldBounds = true;
@@ -31,5 +40,11 @@ function create() {
   ball.body.bounce.set(1);
 
   ball.body.velocity.set(150, 150);
+
+  //Prevent the ball from pushing the paddle
+  paddle.body.immovable = true;
 }
-function update() {}
+function update() {
+  //Make the ball bounce off the paddle
+  game.physics.arcade.collide(ball, paddle);
+}
